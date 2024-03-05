224b8d6046a02f2a42a29c6d6d63e06f133f78ce
Fa Wren Chong <fwc14@uclive.ac.nz>
2024-03-05 20:58:08 +1300

    Made the token take from environment variables.

--- constant.py
+++ constant.py
@@ -1,4 +1,7 @@
-# TODO remove token before commit
-TOKEN = "TOKEN"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.environ['TOKEN'] # create a file called '.env' and put token there. Needs python-dotenv package
 
 BASE_URL = "https://api.github.com/"
