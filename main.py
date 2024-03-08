1448aa85cc19fda44ed06163db458e9e40687ee2
Fa Wren Chong <fwc14@uclive.ac.nz>
2024-03-08 14:43:54 +1300

    Added function to get repositories from a .txt file

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ issue = rep.get_issue(28)
 issue.json_data['number'] = 1
 # print(issue.json_data)
 issue.get_linked_commit()
+
+def get_repositories(path):
+    """Reads a list of repository ids in the format: 
+    "name,owner"
+    and returns a list of Repository objects
+    """
+    repos_file = open(path, 'r')
+    repo_strings = [tuple(repo.split(',')) for repo in repos_file.read().splitlines()]
+    repositories = [Repository(name, owner) for name, owner in repo_strings]
+    return repositories
