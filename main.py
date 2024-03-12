0246dbc6c5dc6cfb8753074c73baecbcecca2290
dlo54 <dlo54@uclive.ac.nz>
2024-03-13 12:57:20 +1300

    added a list of posable repostories

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from Repository import Repository
 # ---------------------------------
 # tensorflow: type:bug
 # pandus: bug
+# ansible: bug,
+# scikit-learn: bug
+# numpy: 00+-+bug
+# matplotlib: status%3A+confirmed+bug
+# scrapy: bug
 # ---------------------------------
 
 rep = Repository('tensorflow', 'tensorflow', "type:bug")
