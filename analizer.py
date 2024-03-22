f9f6bbf3f8edb883d76fe07e38788127a0c354e0
dlo54 <dlo54@uclive.ac.nz>
2024-03-22 17:04:20 +1300

    did start of trimming (can't get trimming to work)

--- analizer.py
+++ analizer.py
@@ -1,10 +1,15 @@
 import glob
 import os
+from copy import copy
 from random import randint
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import spearmanr
+from scipy import stats
+
+def trimmed(arr, percent=10):
+    pass
 
 
 def get_file_data(file_name, shuffle):
@@ -37,7 +42,19 @@ def get_file_data(file_name, shuffle):
     if shuffle:
         np.random.shuffle(times)
 
-    return avs, maxs, totals, ratios, times
+    avt = np.array([avs, copy(times)]).transpose()
+    avt = trimmed(avt, 0.1)
+
+    maxst = np.array([maxs, copy(times)]).transpose()
+    maxst = trimmed(maxst, 0.1)
+
+    totalst = np.array([totals, copy(times)]).transpose()
+    totalst = trimmed(totalst, 0.1)
+
+    ratiost = np.array([ratios, copy(times)]).transpose()
+    ratiost = trimmed(ratiost, 0.1)
+
+    return avt, maxst, totalst, ratiost
 
 
 VALUE_NAMES = ['Average Cyclomatic Complexity', 'Max Cyclomatic Complexity',
@@ -66,18 +83,18 @@ def spear_all(inputs):
 def plot_all(inputs, base='base', shuffle=False):
     data = []
     for file, name in inputs:
-        avs, maxs, totals, ratios, times = get_file_data(file, shuffle)
-        data.append((name, times, (avs, maxs, totals, ratios)))
+        array = get_file_data(file, shuffle)
+        data.append((name, array))
 
     for i in range(4):
         plt.figure(figsize=(10, 10))
         plt.title(f'{VALUE_NAMES[i]} vs Time')
         plt.ylabel("Time (hours)")
         plt.xlabel(VALUE_NAMES[i])
-        for name, times, array in data:
-            values = array[i]
+        for name, array in data:
+            values = array[i].transpose()
             # plt.hist2d(values, times, label=name)
-            plt.scatter(values, times, label=name)
+            plt.scatter(values[0], values[1], label=name)
         plt.legend(loc="upper right")
         plt.savefig(f"{base}_file_{i}.png")
         plt.clf()
@@ -98,7 +115,6 @@ def lineup(inputs, count):
     for f in files:
         os.remove(f)
     for i in range(count):
-
         num = randint(0, 999999999)
         plot_all(inputs, f'lineup/{num}', shuffle=True)
     num = randint(0, 999999999)
@@ -106,6 +122,7 @@ def lineup(inputs, count):
     f = open("answer.txt", "w")
     f.write(str(num))
     f.close()
+    print('done')
 
 
 lineup([('numpy.csv', 'numpy'), ('tensorflow.csv', 'tensorflow'),
